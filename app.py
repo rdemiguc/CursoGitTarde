09790276667fdeed686910b6bbc6e25b6da28e81
angeles <mariadelosangeles.plazagutierrez@gruposantander.es>
2026-03-18 17:55:42 +0100

    h

--- app.py
+++ app.py
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
 print(hola main)
-=======
 print("Hola Mundo")
-<<<<<<< HEAD
-<<<<<<< HEAD
-linea nueva PF
-linea nueva PF
-=======
 print("Hola desde main")
->>>>>>> fa26fe8a6c79da21261462fa0f3b7b35a8049791
-=======
 print("Hola Mundo")
->>>>>>> d07d94a3d1dbff8fe15e86fca1d97a9b34e6627f
->>>>>>> 4dc1268179f1a6f774016acd646322357fd78294
+print(hola angeles)
