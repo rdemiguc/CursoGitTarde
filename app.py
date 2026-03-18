22a8eee6d5f14a34ac2893cff8d4b751c32c3453
JORDA BALERI JOSEP <n810209@gruposantander.es>
2026-03-18 17:55:13 +0100

    pull request

--- app.py
+++ app.py
@@ -1,1 +1,19 @@
+<<<<<<< HEAD
 print("Hola desde main")
+=======
+<<<<<<< HEAD
+print(hola main)
+=======
+print("Hola Mundo")
+<<<<<<< HEAD
+<<<<<<< HEAD
+linea nueva PF
+linea nueva PF
+=======
+print("Hola desde main")
+>>>>>>> fa26fe8a6c79da21261462fa0f3b7b35a8049791
+=======
+print("Hola Mundo")
+>>>>>>> d07d94a3d1dbff8fe15e86fca1d97a9b34e6627f
+>>>>>>> 4dc1268179f1a6f774016acd646322357fd78294
+>>>>>>> 2470771e70484431a2d5269131a2721d7271099a
